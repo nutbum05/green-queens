9fd3f39d1cfc2b74ddba4599f6254ded841e65b7
nutbum05 <44344335+nutbum05@users.noreply.github.com>
2019-10-26 13:25:49 +0100

    Add files via upload
    created footer component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import NavBar from './Nav.js';
+import Footer from '../components/Footer';
+import '../styles/App.css'
+import '../styles/Footer.css'
+
+
+const App = () => {
+  return (
+    <div>
+        
+        <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CoverPhotos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CoverPhotos = props => (
+  <div>
+    <div className="photo1"><img/></div>
+    <div className="photo2"><img/></div>
+    <div className="photo3"><img/></div>
+    <div className="photo4"><img/></div>
+    <div className="photo5"><img/></div>
+    <div className="photo6"><img/></div>
+  </div>
+);
+
+export default CoverPhotos
